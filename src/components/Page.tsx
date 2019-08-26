8ea5f87e6d6316a303c75ff532952dddcfc20e73
Joshua Wong <joshwonghc@gmail.com>
2019-08-26 09:57:40 +0800

    Update page

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,6 +6,10 @@ interface Props {children?: any; }
 
 // From: https://github.com/Microsoft/TypeScript/issues/6471
 export class Page extends React.PureComponent<Props, {}> {
+  constructor(props: Props, {}) {
+    super(props);
+  }
+
   render() {
     return (
       <div className= "page">
